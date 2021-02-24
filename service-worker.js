322306a7480f859baf994baf3a61ac698dcc7078
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2021-02-24 09:20:46 +0700

    ts

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+
+// https://developers.google.com/web/fundamentals/getting-started/primers/service-workers
+// ------------------------------
+// Pre Cache and Update
+// ------------------------------
+
+// const workboxSW = new WorkboxSW({ clientsClaim: true });
+// workboxSW.router
+// workboxSW.precache([]);
+
+workbox.routing.registerRoute(
+    ({request})=> request.destination =='image',
+    new workbox.strategies.CacheFirst()
+)
