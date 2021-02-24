668dbb497d3da918dca100a6f02dea323cbf2d4f
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2021-02-24 15:43:50 +0700

    dfsd

--- service-worker.js
+++ service-worker.js
@@ -10,6 +10,6 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox
 // workboxSW.precache([]);
 
 workbox.routing.registerRoute(
-    ({request})=> request.destination =='image',
-    new workbox.strategies.NetworkFirst()
+    ({request})=> request.destination ==='image',
+    new workbox.strategies.CacheFirst()
 );
