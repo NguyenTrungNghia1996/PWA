24ec19b773bd2645c33f976e207116aad8686483
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2021-03-02 14:06:14 +0700

    te

--- app.js
+++ app.js
@@ -113,6 +113,7 @@ img.addEventListener('click', (e) => {
     deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
             alert('thanh cong')
+            setCookiePWA('isPWA', '1', 365);
         }else {
             deferredPrompt = null;
             alert('no')
@@ -122,3 +123,10 @@ img.addEventListener('click', (e) => {
 window.addEventListener('appinstalled', (evt) => {
     app.logEvent('a2hs', 'installed');
 });
+function setCookiePWA(cname, cvalue, exdays) {
+    let d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
