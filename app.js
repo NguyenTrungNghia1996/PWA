99d105da7b420979d9f8a605ca14c184898eaf6a
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2021-03-02 15:35:01 +0700

    df

--- app.js
+++ app.js
@@ -24,39 +24,6 @@ function manifest(short_name, name, description, icon_192, icon_512, scope, star
         "start_url": start_url+"/"+name,
         "display": "standalone",
         "orientation": "any",
-        "shortcut_item": {
-            "type": "object",
-            "description": "A shortcut item represents a link to a key task or page within a web app. A user agent can use these values to assemble a context menu to be displayed by the operating system when a user engages with the web app's icon.",
-            "properties": {
-                "name": {
-                    "description": "The name member of a shortcut item is a string that represents the name of the shortcut as it is usually displayed to the user in a context menu.",
-                    "type": "string"
-                },
-                "short_name": {
-                    "description": "The short_name member of a shortcut item is a string that represents a short version of the name of the shortcut. It is intended to be used where there is insufficient space to display the full name of the shortcut.",
-                    "type": "string"
-                },
-                "description": {
-                    "description": "The description member of a shortcut item is a string that allows the developer to describe the purpose of the shortcut.",
-                    "type": "string"
-                },
-                "url": {
-                    "description": "The url member of a shortcut item is a URL within scope of a processed manifest that opens when the associated shortcut is activated.",
-                    "type": "string"
-                },
-                "icons": {
-                    "description": "The icons member of a shortcut item serves as iconic representations of the shortcut in various contexts.",
-                    "type": "array",
-                    "items": {
-                        "$ref": "#/definitions/manifest_image_resource"
-                    }
-                }
-            },
-            "required": [
-                "name",
-                "url"
-            ]
-        }
     };
 }
 
