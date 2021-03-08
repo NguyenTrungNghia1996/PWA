118dd73a1bf061ac1f14eaceea21a10b036b5e23
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2021-03-08 14:06:50 +0700

    dfs

--- public/scripts/sw.js
+++ public/scripts/sw.js
@@ -1,18 +1,5 @@
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
-// workbox.routing.registerRoute(
-//     ({request})=> request.destination ==='image',
-//     new workbox.strategies.CacheFirst()
-// );
-// https://developers.google.com/web/fundamentals/getting-started/primers/service-workers
-// ------------------------------
-// Pre Cache and Update
-// ------------------------------
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
-
-const workboxSW = new WorkboxSW({ clientsClaim: true });
-/**
- * precache() is passed a manifest of URLs and versions
- * each time the service worker starts up.
- * Use workbox-build to generate the manifest
- */
-workboxSW.precache([]);
+workbox.routing.registerRoute(
+    ({request})=> request.destination ==='image',
+    new workbox.strategies.CacheFirst()
+);
