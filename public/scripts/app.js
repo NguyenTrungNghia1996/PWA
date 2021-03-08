9b0b1f46fb142c63e0c6c2bce1dd9a6c670b788a
Nguyễn Trung Nghĩa <59058840+NguyenTrungNghia1996@users.noreply.github.com>
2021-03-08 13:43:09 +0700

    sd

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,6 @@
 function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('https://atdsf.herokuapp.com/public/scripts/sw.js').then(reg => {
+        navigator.serviceWorker.register('/sw.js').then(reg => {
             console.log('Registration successful', reg);
         })
             .catch(e => console.error('Error during service worker registration:', e));
